e9d33d23c886300db6482242c66946806c785355
Mridul <mridulbhawa@gmail.com>
2022-09-20 22:01:25 +0530

    created the getter and setter methods for the fields

--- src/main/java/model/PlaylistModel.java
+++ src/main/java/model/PlaylistModel.java
@@ -16,4 +16,20 @@ public class PlaylistModel {
         this.playlistId = playlistId;
         this.playlistName = playlistName;
     }
+
+    public int getPlaylistId() {
+        return playlistId;
+    }
+
+    public void setPlaylistId(int playlistId) {
+        this.playlistId = playlistId;
+    }
+
+    public String getPlaylistName() {
+        return playlistName;
+    }
+
+    public void setPlaylistName(String playlistName) {
+        this.playlistName = playlistName;
+    }
 }
