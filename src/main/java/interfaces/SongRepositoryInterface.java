1ef1aaa581a3f383f37500db1c6eff40cdc0f59a
Mridul <mridulbhawa@gmail.com>
2022-09-21 12:54:43 +0530

    created the interfaces of the methods to be implemented in SongRepository

--- src/main/java/interfaces/SongRepositoryInterface.java
+++ src/main/java/interfaces/SongRepositoryInterface.java
@@ -1,4 +1,13 @@
 package interfaces;
 
+import model.SongModel;
+
+import java.util.List;
+
 public interface SongRepositoryInterface {
-}
+    List<SongModel> displayAllSongs();
+    List<SongModel> searchBySongName(String songName);
+    List<SongModel> searchByArtistName(String artist);
+    List<SongModel> searchByGenre(String genre);
+    }
+
