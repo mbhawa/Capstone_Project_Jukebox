ca42cf27756aa2c72bf7ff8a864783acd84a62d4
Mridul <mridulbhawa@gmail.com>
2022-09-21 10:33:05 +0530

    added a constructor for the Connection

--- src/main/java/service/DatabaseService.java
+++ src/main/java/service/DatabaseService.java
@@ -5,8 +5,15 @@
  */
 package service;
 
+import java.sql.Connection;
+
 public class DatabaseService {
     public static final String URL="jdbc:mysql://localhost:3306/jukebox";
     public static final String USERNAME="root";
     public static final String PASSWORD="rootpassword";
+    private Connection connection;
+
+    public DatabaseService(Connection connection) {
+        this.connection = connection;
+    }
 }
