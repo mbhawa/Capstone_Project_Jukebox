a14f88a82c207df4693f35eb7da37604f9cf33f7
Mridul <mridulbhawa@gmail.com>
2022-09-21 09:49:49 +0530

    added a query to create a table song

--- src/main/resources/sql_queries/jukebox_database_queries.sql
+++ src/main/resources/sql_queries/jukebox_database_queries.sql
@@ -4,3 +4,12 @@ create database if not exists `jukebox`;
 -- use database `jukebox`
 use `jukebox`;
 
+-- create a table `song_name`
+create table if not exists `song_details`(
+`song_id` int not null primary key auto_increment,
+`song_name` varchar(30) not null,
+`album_name` varchar(30) not null,
+`duration` time not null,
+`genre` varchar(20) not null,
+`song_path` varchar(60) not null
+);
