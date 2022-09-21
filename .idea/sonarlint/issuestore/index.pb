c8be515766eec4d51050e6dd083dd9aede987304
Mridul <mridulbhawa@gmail.com>
2022-09-21 12:47:01 +0530

    refactored the DatabaseService.java class

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -10,3 +10,5 @@ w
 G.idea/sonarlint/issuestore/8/a/8a6dae537be9f47878c6964c6ee40cab0ca75e28,8\e\8e3d5572111947a6fc62da316a319faf3e927047
 7
 pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+w
+G.idea/sonarlint/issuestore/4/4/442292b8a7efeabbe4cc176709b833b1792140ec,8\6\863e0292fd7e9b3e74dafd6cc01f73ba76346482
--- src/main/java/service/DatabaseService.java
+++ src/main/java/service/DatabaseService.java
@@ -10,14 +10,18 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseService {
+    //create fields for the class DatabaseService
     public static final String URL="jdbc:mysql://localhost:3306/jukebox";
     public static final String USERNAME="root";
     public static final String PASSWORD="rootpassword";
     private Connection connection;
 
+    //create a constructor and initialise the connection as  null
     public DatabaseService(Connection connection) {
-        this.connection = connection;
+        this.connection = null;
     }
+
+    //create a method to connect the database
     public void connect(Connection connection) throws SQLException {
         connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
     }
